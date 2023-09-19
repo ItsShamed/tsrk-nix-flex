376739fdbd98463abb012f3cca7e0cd744c06fda
tsrk. <tsrk@tsrk.me>
2023-09-19 11:23:20 +0200

    chore: formatting

--- modules/system/packages/pkgs/desktop.nix
+++ modules/system/packages/pkgs/desktop.nix
@@ -2,43 +2,47 @@
 
 let
   tsrk-librewolf = with pkgs;
-      (wrapFirefox librewolf-unwrapped {
-        nixExtensions = [
-          (fetchFirefoxAddon {
-            name = "CanvasBlocker";
-            url = "https://addons.mozilla.org/firefox/downloads/file/4097901/canvasblocker-1.9.xpi";
-            hash = "";
-          })
-          (fetchFirefoxAddon {
-            name = "SponsorBlock";
-            url = "https://addons.mozilla.org/firefox/downloads/file/4163966/sponsorblock-5.4.19.xpi";
-            hash = "";
-          })
-          (fetchFirefoxAddon {
-            name = "DeArrow";
-            url = "https://addons.mozilla.org/firefox/downloads/file/4163352/dearrow-1.2.17.xpi";
-            hash = "";
-          })
-          (fetchFirefoxAddon {
-            name = "ToSDR";
-            url = "https://addons.mozilla.org/firefox/downloads/file/3827536/terms_of_service_didnt_read-4.1.2.xpi";
-            hash = "";
-          })
-        ];
+    (wrapFirefox librewolf-unwrapped {
+      nixExtensions = [
+        (fetchFirefoxAddon {
+          name = "CanvasBlocker";
+          url =
+            "https://addons.mozilla.org/firefox/downloads/file/4097901/canvasblocker-1.9.xpi";
+          hash = "";
+        })
+        (fetchFirefoxAddon {
+          name = "SponsorBlock";
+          url =
+            "https://addons.mozilla.org/firefox/downloads/file/4163966/sponsorblock-5.4.19.xpi";
+          hash = "";
+        })
+        (fetchFirefoxAddon {
+          name = "DeArrow";
+          url =
+            "https://addons.mozilla.org/firefox/downloads/file/4163352/dearrow-1.2.17.xpi";
+          hash = "";
+        })
+        (fetchFirefoxAddon {
+          name = "ToSDR";
+          url =
+            "https://addons.mozilla.org/firefox/downloads/file/3827536/terms_of_service_didnt_read-4.1.2.xpi";
+          hash = "";
+        })
+      ];
 
-        extraPrefs = ''
-          defaultPref("pricacy.resistFingerprinting", false);
-          defaultPref("privacy.resistFingerprinting.letterboxing", true);
-          defaultPref("privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts", true);
-          defaultPref("identity.fxaccounts.enabled", true);
-          defaultPref("extensions.update.autoUpdateDefault", true);
-          defaultPref("extensions.update.enabled", true);
-        '';
-      });
-in
-{
+      extraPrefs = ''
+        defaultPref("pricacy.resistFingerprinting", false);
+        defaultPref("privacy.resistFingerprinting.letterboxing", true);
+        defaultPref("privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts", true);
+        defaultPref("identity.fxaccounts.enabled", true);
+        defaultPref("extensions.update.autoUpdateDefault", true);
+        defaultPref("extensions.update.enabled", true);
+      '';
+    });
+in {
   options = {
-    tsrk.packages.pkgs.desktop.enable = lib.mkEnableOption "tsrk's desktop bundle";
+    tsrk.packages.pkgs.desktop.enable =
+      lib.options.mkEnableOption "tsrk's desktop bundle";
   };
 
   config = lib.mkIf config.tsrk.packages.pkgs.desktop.enable {
