9fb230a84699ce407758cda2ac39b1376521a935
tsrk. <tsrk@tsrk.me>
2023-09-23 14:10:57 +0200

    feat(network): allow hostname customisation

---
+++ modules/system/network/hostname.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    tsrk.networking.hostname = {
+      useDHCPHostname = lib.options.mkOption {
+        type = lib.types.bool;
+        description = "Whether to use the hostname provided by DHCP.";
+        default = false;
+      };
+      base = lib.options.mkOption {
+        type = lib.types.string;
+        description = "The base hostname to use.";
+        default = "tsrk";
+      };
+      removeImageSuffix = lib.option.mkOption {
+        type = lib.types.bool;
+        description = "Whether to remove the suffix identifying the image you are using.";
+        default = false;
+      };
+    };
+  };
+
+  config = lib.mkIf tsrk.networking.hostname.useDHCPHostname {
+    networking.hostName = lib.mkForce "";
+  };
+}
