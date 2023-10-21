7fa88dc6402e468adbcbcc6ca4630f96c838a042
tsrk. <tsrk@tsrk.me>
2023-10-21 16:21:06 +0200

    refactor(packages/pkgs/desktop): remove non-essential packages
    To make the initial bootstrap lighter

--- modules/system/packages/pkgs/desktop.nix
+++ modules/system/packages/pkgs/desktop.nix
@@ -3,41 +3,7 @@
   lib,
   pkgs,
   ...
-}: let
-  tsrk-librewolf = with pkgs; (wrapFirefox librewolf-unwrapped {
-    nixExtensions = [
-      (fetchFirefoxAddon {
-        name = "CanvasBlocker";
-        url = "https://addons.mozilla.org/firefox/downloads/file/4097901/canvasblocker-1.9.xpi";
-        hash = "";
-      })
-      (fetchFirefoxAddon {
-        name = "SponsorBlock";
-        url = "https://addons.mozilla.org/firefox/downloads/file/4163966/sponsorblock-5.4.19.xpi";
-        hash = "";
-      })
-      (fetchFirefoxAddon {
-        name = "DeArrow";
-        url = "https://addons.mozilla.org/firefox/downloads/file/4163352/dearrow-1.2.17.xpi";
-        hash = "";
-      })
-      (fetchFirefoxAddon {
-        name = "ToSDR";
-        url = "https://addons.mozilla.org/firefox/downloads/file/3827536/terms_of_service_didnt_read-4.1.2.xpi";
-        hash = "";
-      })
-    ];
-
-    extraPrefs = ''
-      defaultPref("pricacy.resistFingerprinting", false);
-      defaultPref("privacy.resistFingerprinting.letterboxing", true);
-      defaultPref("privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts", true);
-      defaultPref("identity.fxaccounts.enabled", true);
-      defaultPref("extensions.update.autoUpdateDefault", true);
-      defaultPref("extensions.update.enabled", true);
-    '';
-  });
-in {
+}: {
   options = {
     tsrk.packages.pkgs.desktop.enable =
       lib.options.mkEnableOption "tsrk's desktop bundle";
@@ -45,24 +11,17 @@ in {
 
   config = lib.mkIf config.tsrk.packages.pkgs.desktop.enable {
     environement.systemPackages = with pkgs; [
-      # browser
-      tsrk-librewolf
-
       # communication
       thunderbird
       weechat
-      armcord
 
       # images
       feh
-      gimp
       imagemagick
       scrot
 
       # video
-      vlc
       mpv
-      libsForQt5.kdenlive
 
       # cli
       kitty
@@ -74,9 +33,6 @@ in {
       xorg.xkill
       xsel
       x11vnc
-
-      # PDF
-      zathura
     ];
   };
 }
