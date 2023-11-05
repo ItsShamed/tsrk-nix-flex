d46d5916b59c478b39eeb95be5130ace31165075
tsrk. <tsrk@tsrk.me>
2023-11-05 23:54:13 +0100

    refactor(bluetooth): use full bluez package

--- modules/system/services/hardware/bluetooth.nix
+++ modules/system/services/hardware/bluetooth.nix
@@ -6,7 +6,10 @@
   };
 
   config = lib.mkIf config.tsrk.bluetooth.enable {
-    hardware.bluetooth.enable = true;
+    hardware.bluetooth = {
+      enable = true;
+      package = pkgs.bluezFull
+    };
     services.blueman.enable = true;
 
     environment.systemPackages = with pkgs; [
