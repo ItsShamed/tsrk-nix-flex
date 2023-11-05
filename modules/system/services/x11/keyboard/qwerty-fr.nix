de8e206c88d5bd83ed960e78469e1c7e45bf07a8
tsrk. <tsrk@tsrk.me>
2023-11-05 23:13:37 +0100

    feat(services/x11): add qwerty-fr keyboard layout

---
+++ modules/system/services/x11/keyboard/qwerty-fr.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+
+let
+  qwerty-fr = builtins.fetchGit {
+    url = "https://github.com/qwerty-fr/qwerty-fr.git";
+    rev = "3ac6e889a46e0c0aad44df2a0d23b3d8fe1257d5";
+    ref = "refs/tags/v0.7.2";
+  };
+in
+{
+  options = {
+    tsrk.qwerty-fr = {
+      enable = lib.options.mkEnableOption "the French QWERTY keyboard layout";
+    };
+  };
+
+  config = lib.mkIf config.tsrk.qwerty-fr.enable {
+    services.xserver.extraLayouts.us_qwerty-fr = {
+      description =
+        "QWERTY-based layout. Type EU languages, greek, math, currencies, & more!";
+      languages = [ "eng" "fra" "deu" "spa" "grc" ];
+      symbolsFile = "${qwerty-fr}/linux/us_qwerty-fr";
+    };
+  };
+}
