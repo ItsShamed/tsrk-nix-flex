6695942086043eef0ae7aa637db1ff1e2e03968c
tsrk. <tsrk@tsrk.me>
2023-11-06 17:42:27 +0100

    feat(profiles): create basic core profile

---
+++ profiles/core/default.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  i18n.defaultLocale = "en.UTF-8";
+  i18.extraLocaleSettings = {
+    LANGUAGE = "en:en_US:C:fr_FR";
+    LC_TIME = "en_DK.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+  };
+
+  time.timeZone = "Europe/Paris";
+
+  console.keyMap = "us";
+
+  nix = {
+    package = pkgs.nixFlakes;
+
+    settings = {
+      trusted-users = [ "root" "@wheel" ];
+      substituters = [
+        "https://s3.cri.epita.fr/cri-nix-cache.s3.cri.epita.fr" # We never know, I use their flake so we might as well reduce the network overhead
+        "https://nix-gaming.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nix.cri.epita.fr:qDIfJpZWGBWaGXKO3wZL1zmC+DikhMwFRO4RVE6VVeo="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      ];
+    };
+
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  hardware.enableRedistributableFirmware = true;
+
+}
