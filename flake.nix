1c7d6a4750e2e480b852982bf0bd62fbc8943ef4
tsrk. <tsrk@tsrk.me>
2023-11-08 14:11:36 +0100

    chore: add nixvim and home-manager inputs

--- flake.nix
+++ flake.nix
@@ -32,6 +32,16 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgsUnstable"
+    }
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgsUnstable"
+    }
   };
 
   outputs =
