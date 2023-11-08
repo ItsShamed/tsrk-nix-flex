fb40679f65ca52d0b30e2e9690f5f87857a2746a
tsrk. <tsrk@tsrk.me>
2023-11-08 14:11:18 +0100

    feat(lib/generateUser): add ability to make user do sudo

--- lib/generateUser.nix
+++ lib/generateUser.nix
@@ -3,13 +3,18 @@ name:
 {
   password ? null,
   passwordFile ? null,
-  initialPassword ? ""
+  initialPassword ? "",
+  canSudo ? false,
 }:
+
+{ lib, ... }:
 
 {
   users.users.${name} = {
     inherit name;
     inherit passwordFile password initialPassword;
     isNormalUser = true;
+    extraGroups = [ "video" "audio" ]
+      ++ (lib.lists.optional canSudo "wheel");
   };
 }
