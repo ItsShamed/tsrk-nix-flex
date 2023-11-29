e561562fca45eaa9eec5d1fe60e69d1ba39f0643
tsrk. <tsrk@tsrk.me>
2023-11-29 14:54:45 +0100

    style: remember nix syntax

--- modules/system/services/hardware/disks.nix
+++ modules/system/services/hardware/disks.nix
@@ -1,9 +1,9 @@
-[ config, lib, ... ]:
+{ config, lib, ... }:
 
 {
   options = {
     tsrk.disk-management.enable = lib.options.mkEnableOption "disk managment";
-  }
+  };
 
   config = lib.mkIf config.tsrk.disk-management.enable {
     services.udisks2.enable = lib.mkDefault true;
