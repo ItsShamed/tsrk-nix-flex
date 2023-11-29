2e8672fc74db292a73244ed3098452766015c628
tsrk. <tsrk@tsrk.me>
2023-11-29 14:54:45 +0100

    style: remember nix syntax

--- modules/system/services/x11/sessions/hyprland.nix
+++ modules/system/services/x11/sessions/hyprland.nix
@@ -3,9 +3,9 @@
 {
   options = {
     tsrk.hyprland.enable = lib.options.mkEnableOption "Hyprland as a window manager";
-  }
+  };
 
   config = lib.mkIf config.tsrk.hyprland.enable {
     programs.hyprland.enable = true;
-  }
+  };
 }
