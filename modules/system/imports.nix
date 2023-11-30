7177b5f56fa04651a4b9d19c03a179494132a4f6
tsrk. <tsrk@tsrk.me>
2023-11-30 23:57:23 +0100

    refactor(modules/system): move redshift module to x11 folder

--- modules/system/imports.nix
+++ modules/system/imports.nix
@@ -5,10 +5,10 @@
   ./services/hardware/bluetooth.nix
   ./services/hardware/disks.nix
   ./services/networking/sshd
-  ./services/redshift.nix
   ./services/virtualisation/containers
   ./services/x11/dipslay-manager/sddm.nix
   ./services/x11/keyboard/qwerty-fr.nix
+  ./services/x11/redshift.nix
   ./services/x11/sessions/i3.nix
   ./services/x11/sessions/hyprland.nix
 ]
