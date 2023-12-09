e747bc1431a2f670209bcce78778f28f4691510e
tsrk. <tsrk@tsrk.me>
2023-12-09 18:24:39 +0100

    refactor: make profiles not depend on each other

--- hosts/test.nix
+++ hosts/test.nix
@@ -2,6 +2,8 @@
 
 {
   imports = [
+    self.nixosModules.profile-base
+    self.nixosModules.profile-graphical-base
     self.nixosModules.profile-graphical-x11
     (self.lib.generateUser "test" { password = "quoicoubeh"; canSudo = true; })
   ];
