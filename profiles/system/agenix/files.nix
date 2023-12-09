39e1338c4f6e2fca5a1a486973063a87d1fc4acf
tsrk. <tsrk@tsrk.me>
2023-12-09 17:45:38 +0100

    fix(agenix): fix SSH private keys path

--- profiles/system/agenix/files.nix
+++ profiles/system/agenix/files.nix
@@ -7,11 +7,11 @@
 
   age.secrets = {
     ssh_host_ed25519_key = {
-      file = ./ssh_host_ed25519_key.age;
+      file = ./files/ssh_host_ed25519_key.age;
       mode = "600";
     };
     ssh_host_rsa_key = {
-      file = ./ssh_host_rsa_key.age;
+      file = ./files/ssh_host_rsa_key.age;
       mode = "600";
     };
   };
