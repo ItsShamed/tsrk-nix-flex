b80fe56c8be98f28dd0dbdc64b92602a86a23c9e
tsrk. <tsrk@tsrk.me>
2023-12-09 22:49:12 +0100

    feat(packages/pkgs): add module for C development

---
+++ modules/system/packages/pkgs/cDev.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    tsrk.packages.pkgs.cDev.enable = lib.options.mkEnableOption "tsrk's C development bundle";
+  };
+
+  config = lib.mkIf config.tsrk.packages.pkgs.cDev.enable {
+    environment.systemPackages = with pkgs; [
+      autoconf
+      autoconf-archive
+      automake
+      cmake
+      gnumake
+      meson
+      ninja
+
+      gcc
+      (lib.setPrio (gcc.meta.priority + 1) gcc-unwrapped)
+
+      criterion
+      gtest
+      gcovr
+
+      clang-tools
+      dash
+      doxygen
+      flex
+      gdb
+      lcov
+      ltrace
+      pkg-config
+      readline
+      tk
+      valgrind
+
+      perlPackages.JSON
+      perlPackages.PerlIOgzip
+    ];
+  };
+}
