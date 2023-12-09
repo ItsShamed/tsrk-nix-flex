76b96b0a198fc61820a6114f06eb1f2e361dd236
tsrk. <tsrk@tsrk.me>
2023-12-09 18:25:29 +0100

    feat(hosts/test): import hostname module

--- hosts/test.nix
+++ hosts/test.nix
@@ -5,6 +5,7 @@
     self.nixosModules.profile-base
     self.nixosModules.profile-graphical-base
     self.nixosModules.profile-graphical-x11
+    self.nixosModules.hostname
     (self.lib.generateUser "test" { password = "quoicoubeh"; canSudo = true; })
   ];
 
