0f1a6ff05dbc36ebe95287280107b6154f931f16
tsrk. <tsrk@tsrk.me>
2023-12-13 10:13:51 +0100

    feat(hosts): add installer image

---
+++ hosts/genesis/default.nix
@@ -0,0 +1,28 @@
+{ self, config, lib, ... }:
+
+{
+  imports = [
+    self.nixosModules.profile-agenix
+    self.nixosModules.profile-base
+    self.nixosModules.profile-graphical-base
+    self.nixosModules.profile-graphical-x11
+    self.nixosModules.hostname
+  ];
+
+  tsrk.sshd.customKeyPair = {
+    enable = true;
+    rsa = {
+      private = ../.tsrk-files/ssh_host_rsa_key;
+      public = ../.tsrk-files/ssh_host_rsa_key.pub;
+    };
+    ed25519 = {
+      private = ../.tsrk-files/ssh_host_ed25519_key;
+      public = ../.tsrk-files/ssh_host_ed25519_key.pub;
+    };
+  };
+
+  age.identityPaths = lib.mkOptionDefault [
+    ../.tsrk-files/ssh_host_ed25519_key
+    ../.tsrk-files/ssh_host_rsa_key
+  ];
+}
--- hosts/hosts.nix
+++ hosts/hosts.nix
@@ -1,4 +1,5 @@
 [
   ./test.nix
   ./chiisai
+  ./genesis
 ]
