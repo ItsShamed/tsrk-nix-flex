2aca77e7e94e710a6f00b2db763b86c0bccdb246
tsrk. <tsrk@tsrk.me>
2024-03-17 23:14:43 +0100

    feat(home: tsrk: extra-packages): add csharp dev tools
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- homes/tsrk/extra-packages.nix
+++ homes/tsrk/extra-packages.nix
@@ -4,9 +4,14 @@
   home.packages = with pkgs; [
     openjdk17
     jetbrains.idea-ultimate
+    dotnet-sdk_8
+    mono
+    jetbrains.rider
   ];
 
   home.sessionVariables = {
     JAVA_HOME = "${pkgs.openjdk17}/lib/openjdk";
+    DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
+    DOTNET_CLI_TELEMETRY_OPTOUT = "true";
   };
 }
