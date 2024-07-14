b56baa2a614dbc60aab254838df561e796250b37
tsrk. <tsrk@tsrk.me>
2024-07-14 22:13:47 +0200

    refactor(modules: home: picom)!: remove animations for now
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/home/desktop/picom.nix
+++ modules/home/desktop/picom.nix
@@ -4,7 +4,6 @@ let
   baseConfig = lib.mkIf config.tsrk.i3.enable {
     services.picom = {
       # FIXME: find better alternative, this fork will be deleted anytime soon
-      package = pkgs.picom-allusive;
       enable = true;
       fade = true;
       extraArgs = [ "--no-use-damage" ];
@@ -19,44 +18,6 @@ let
       shadowExclude = [
         "class_g = 'Dunst'"
       ];
-      settings = {
-        animation-open-exclude = [
-          "class_g = 'i3lock'"
-        ];
-        animation-unmap-exclude = [
-          "class_g = 'i3lock'"
-        ];
-        animation-dampening = 5;
-        animation-stiffnes = 50;
-        animation-window-mass = 0.10;
-        animation-for-unmap-window = "slide-down";
-        wintypes = {
-          notification = {
-            animation = "slide-left";
-            animation-unmap = "slide-right";
-          };
-          dropdown_menu = {
-            animation = "slide-down";
-            animation-unmap = "slide-up";
-          };
-          menu = {
-            animation = "slide-up";
-            animation-unmap = "slide-down";
-          };
-          tooltip = {
-            animation = "slide-up";
-            animation-unmap = "slide-down";
-          };
-          popup_menu = {
-            animation = "zoom";
-            animation-unmap = "zoom";
-          };
-          dialog = {
-            animation = "zoom";
-            animation-unmap = "zoom";
-          };
-        };
-      };
     };
   };
 
