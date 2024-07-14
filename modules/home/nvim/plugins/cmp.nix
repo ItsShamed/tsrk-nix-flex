2826d326e015934a7baeecfded6039837eddbec0
tsrk. <tsrk@tsrk.me>
2024-07-14 16:01:20 +0200

    fix(nvim: cmp: icons): extract icons and use mappings to substitute icons
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/home/nvim/plugins/cmp.nix
+++ modules/home/nvim/plugins/cmp.nix
@@ -2,42 +2,7 @@
 
 with vimHelpers;
 let
-  icons = {
-    Array = "";
-    Boolean = "";
-    Class = "";
-    Color = "";
-    Constant = "";
-    Constructor = "";
-    Enum = "";
-    EnumMember = "";
-    Event = "";
-    Field = "";
-    File = "";
-    Folder = "󰉋";
-    Function = "";
-    Interface = "";
-    Key = "";
-    Keyword = "";
-    Method = "";
-    Module = "";
-    Namespace = "";
-    Null = "󰟢";
-    Number = "";
-    Object = "";
-    Operator = "";
-    Package = "";
-    Property = "";
-    Reference = "";
-    Snippet = "";
-    String = "";
-    Struct = "";
-    Text = "";
-    TypeParameter = "";
-    Unit = "";
-    Value = "";
-    Variable = "";
-  };
+  icons = import ../utils/icons.nix;
 
   sourceNames = {
     nvim_lsp = "(LSP)";
@@ -207,18 +172,29 @@ in
       "function(args) require('luasnip').lsp_expand(args.body) end";
     plugins.cmp.settings.formatting.format = ''
       function(entry, vim_item)
-        local kind_icons = ${toLuaObject icons}
+        local icons = ${toLuaObject icons}
+        local kind_icons = icons.kind
         local source_names = ${toLuaObject sourceNames}
         local duplicates = ${toLuaObject duplicates}
 
         local max_width = 0
         if max_width ~= 0 and #vim_item.abbr > max_width then
-          vim_item.abbr = string.sub(vim_item.abbr, 1, max_width - 1) .. 
+          vim_item.abbr = string.sub(vim_item.abbr, 1, max_width - 1) .. icons.ui.Ellipsis
         end
         vim_item.kind = kind_icons[vim_item.kind]
 
+        if entry.source.name == "crate" then
+          vim_item.kind = icons.misc.Package
+          vim_item.kind_hl_group = "CmpItemKindCrate"
+        end
+
+        if entry.source.name == "lab.quick_data" then
+          vim_item.kind = icons.misc.CircuitBoard
+          vim_item.kind_hl_group = "CmpItemKindConstant"
+        end
+
         if entry.source.name == "emoji" then
-          vim_item.kind = 
+          vim_item.kind = icons.misc.Smiley
           vim_item.kind_hl_group = "CmpItemKindEmoji"
         end
         vim_item.menu = source_names[entry.source.name] or "(" .. entry.source.name .. ")"
