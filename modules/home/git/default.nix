136a4e0459433141be024b371a2d38e499b3855a
tsrk. <tsrk@tsrk.me>
2024-07-16 21:57:01 +0200

    refactor(home: git): remove specific git config and warn about empty email
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -12,15 +12,17 @@
   };
 
   config = lib.mkIf config.tsrk.git.enable {
+    warnings = (lib.mkIf (config.programs.git.userEmail == null) [
+      ''
+        You didn't set an e-mail in your Git config. If it is intended, don't
+        forget to set it manually afterwards. Otherwise, you won't be able to
+        use Git.
+      ''
+    ]);
     programs.git = {
       enable = true;
       lfs.enable = lib.mkDefault true;
-      signing = {
-        signByDefault = lib.mkDefault true;
-        key = lib.mkDefault "D1C2AD054267D54D248A4F43EBD46BB3049B56D6";
-      };
-      userName = lib.mkDefault "tsrk.";
-      userEmail = lib.mkDefault "tsrk@tsrk.me";
+      userName = lib.mkDefault "${config.home.username}";
     };
   };
 }
