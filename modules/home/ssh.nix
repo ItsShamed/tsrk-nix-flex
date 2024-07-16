ada9e06ccf828f7253b8426c251b439897f256c7
tsrk. <tsrk@tsrk.me>
2024-07-16 22:02:10 +0200

    feat(home: ssh): add keys to agent
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -4,7 +4,6 @@
   services.ssh-agent.enable = true;
   programs.ssh = {
     enable = true;
-    # TODO: Enable this when switch to 24.05
-    # addKeysToAgent = "yes";
+    addKeysToAgent = "yes";
   };
 }
