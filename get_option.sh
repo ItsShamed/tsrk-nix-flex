a9af4741f5fba132f683d1eed945c0a93cd161b0
tsrk. <tsrk@tsrk.me>
2024-07-18 19:53:16 +0200

    chore(get_option): allow to pass parameters to nix-option
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- get_option.sh
+++ get_option.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
+
+if [ $# -lt 2 ]; then
     echo "Usage: ./get_option.sh <host> <attrPath>" 1>&2
     exit 1
 fi
 
-CONFIG_PREFIX="let flake = import $(nix flake metadata --json | jq .path -r); in flake.nixosConfigurations.$1"
+host=$1
+shift
+attrPath=$1
+shift
 
-nix run nixpkgs#nixos-option -- --config_expr "${CONFIG_PREFIX}.config" --options_expr "${CONFIG_PREFIX}.options" "$2"
+CONFIG_PREFIX="let flake = import $(nix flake metadata --json | jq .path -r); in flake.nixosConfigurations.$host"
+
+nix run nixpkgs#nixos-option -- --config_expr "${CONFIG_PREFIX}.config" --options_expr "${CONFIG_PREFIX}.options" "$attrPath" $@
