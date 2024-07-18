9dbb5fa3bfb3dd239ce438f56b0cfddba3cb84e0
tsrk. <tsrk@tsrk.me>
2024-07-18 19:41:50 +0200

    refactor(hosts: chiisai): apply profiles changes
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- hosts/chiisai/user.nix
+++ hosts/chiisai/user.nix
@@ -2,48 +2,18 @@
 
 {
   imports = [
-    self.homeManagerModules.i3
-    self.homeManagerModules.polybar
-    self.homeManagerModules.kitty
-    self.homeManagerModules.packages
-    self.homeManagerModules.dunst
-    self.homeManagerModules.git
-    self.homeManagerModules.zsh
-    self.homeManagerModules.starship
-    self.homeManagerModules.bat
-    self.homeManagerModules.nvim
-    self.homeManagerModules.thunderbird
-    self.homeManagerModules.profile-epita-tsrk
-    self.homeManagerModules.xsettingsd
-    self.homeManagerModules.darkman
-    self.homeManagerModules.flameshot
-    self.homeManagerModules.compat
+    self.profile-epita-tsrk
+    self.profile-x11
+    self.profile-git
+    self.profile-shell
   ];
 
   tsrk = {
-    i3 = {
-      enable = true;
-      useLogind = true;
-    };
-    xsettingsd.enable = true;
-    kitty.enable = true;
     darkman = {
-      enable = true;
-      nvim.enable = true;
       feh = {
-        enable = true;
         dark = ./files/bocchi-tokyonight-storm.png;
         light = ./files/lagtrain-tokyonight-day.png;
       };
-    };
-    polybar = {
-      enable = true;
-    };
-    git = {
-      enable = true;
-      cli.enable = true;
-      lazygit.enable = true;
-      delta.enable = true;
     };
   };
 
@@ -76,5 +46,13 @@
       };
       thunderbird.enable = true;
     };
+  };
+
+  programs.git = {
+    signing = {
+      signByDefault = true;
+      key = "D1C2AD054267D54D248A4F43EBD46BB3049B56D6";
+    };
+    userEmail = "tsrk@tsrk.me";
   };
 }
