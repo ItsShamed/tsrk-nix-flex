66f1ad831e3b899068bd9ade5265d958c65fefe3
tsrk <tsrk@tsrk.me>
2024-07-19 20:34:46 +0200

    fix(home: desktop: xsettingsd): just restart the system service
    Figures that if you SIGHUP a daemon instance with a forced config, it
    will reload that same config, duh
    
    Signed-off-by: tsrk <tsrk@tsrk.me>

--- modules/home/desktop/xsettingsd.nix
+++ modules/home/desktop/xsettingsd.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, hmLib, ... }:
+{ config, lib, hmLib, ... }:
 
 let
   cfg = config.tsrk.xsettingsd;
@@ -33,24 +33,7 @@ in
     home.activation.xsettingsd-reload = hmLib.dag.entryAfter [ "reloadSystemd" ] ''
       _i "Reloading xsettingsd"
 
-      verboseEcho "Sending SIGHUP to all xsettingsd instances"
-      if ! ${pkgs.killall}/bin/killall -HUP xsettingsd; then
-        verboseEcho "Reloading xsettingsd systemd service"
-        if ! ${config.systemd.user.systemctlPath} --user restart xsettingsd; then
-          _iWarn "Failed to reload xsettingsd, themes will not be updated"
-        fi
-      else
-        if ! ${config.systemd.user.systemctlPath} --user is-active xsettingsd; then
-          warnEcho "xsettingsd is no longer active, trying to re-enable it"
-          if ! ${config.systemd.user.systemctlPath} --user enable --now xsettingsd; then
-            warnEcho "Failed to enable xsettingsd service, themes will not be updated"
-          fi
-        fi
-        ${pkgs.lxappearance}/bin/lxappearance &
-        ${pkgs.coreutils}/bin/sleep 1
-        ${pkgs.killall}/bin/killall .lxappearance-wrapped
-      fi
-      (sleep 1; ${config.systemd.user.systemctlPath} --user restart xsettingsd)&
+      ${config.systemd.user.systemctlPath} --user restart xsettingsd
     '';
   };
 }
