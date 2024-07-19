e6bb314a1b74e10a11bf4e118c78506d798391be
tsrk. <tsrk@tsrk.me>
2024-07-19 13:27:24 +0200

    feat(pkgs): add rofi-themes-collections
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -7,4 +7,6 @@
     path = ./data/themes/sddm-slice-theme;
     callPackage = self: super: self.libsForQt5.callPackage;
   };
+
+  rofi-themes-collection = ./data/themes/rofi-themes-collection;
 }
---
+++ pkgs/data/themes/rofi-themes-collection/default.nix
@@ -0,0 +1,30 @@
+{ fetchFromGitHub
+, stdenv
+, lib
+}:
+
+stdenv.mkDerivation {
+  pname = "rofi-themes-collection";
+  version = "2024-06-27";
+
+  src = fetchFromGitHub {
+    owner = "newmanls";
+    repo = "rofi-themes-collection";
+    rev = "c8239a45edced3502894e1716a8b661fdea8f1c9";
+    hash = "sha256-1F+qMwchTUWdEWpsIqyVG5pYmqdvmsCckvSmg7pYjdY=";
+  };
+
+  installPhase = ''
+    rm -r screenshots
+    mkdir -p $out
+    mv themes/* $out
+    mv * $out
+  '';
+
+  meta = with lib; {
+    license = licenses.gpl3Plus;
+    homepage = "https://github.com/newmanls/rofi-themes-collection";
+    description = "Themes Collection for Rofi Launcher";
+    platforms = platforms.linux;
+  };
+}
