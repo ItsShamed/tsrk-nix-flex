05638aef77979937e051c08aec5ce4c0ff5a34c2
tsrk. <tsrk@tsrk.me>
2024-07-21 03:32:10 +0200

    feat(system: packages: pkgs): add gaming bundle
    Even though this bundle is more like an entire module, but hey, at least
    we get to play games!!!!
    
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/system/packages/pkgs/default.nix
+++ modules/system/packages/pkgs/default.nix
@@ -7,6 +7,7 @@
     ./csharp.nix
     ./desktop.nix
     ./fs.nix
+    ./gaming.nix
     ./go.nix
     ./java.nix
     ./python.nix
---
+++ modules/system/packages/pkgs/gaming.nix
@@ -0,0 +1,36 @@
+{ lib, config, pkgs, ... }:
+
+{
+  options = {
+    tsrk.packages.pkgs = {
+      gaming = {
+        enable = lib.options.mkEnableOption "tsrk's gaming package bundle";
+        amdSupport = lib.options.mkEnableOption "AMD support";
+      };
+    };
+  };
+
+  config = lib.mkIf config.tsrk.packages.pkgs.gaming.enable (lib.mkMerge [
+    {
+      environment.systemPackages = with pkgs; [
+        gamemode
+      ];
+
+      hardware.steam.enable = lib.mkDefault true;
+      programs.steam = {
+        enable = lib.mkDefault true;
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
+        remotePlay.openFirewall = lib.mkDefault true;
+        localNetworkGameTransfers.openFirewall = lib.mkDefault true;
+      };
+    }
+    (lib.mkIf (config.tsrk.packages.pkgs.gaming.amdSupport) {
+      hardware.opengl = {
+        extraPackages = [ pkgs.amdvlk ];
+        extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+      };
+    })
+  ]);
+}
