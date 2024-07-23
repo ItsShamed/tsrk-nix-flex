80720f806b800399519cdc8796fc360fada5b9d1
tsrk. <tsrk@tsrk.me>
2024-07-23 18:29:45 +0200

    feat(system: servies: hardware: bluetooth): add Litarvan's bluetooth audio improvements
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/system/services/hardware/bluetooth.nix
+++ modules/system/services/hardware/bluetooth.nix
@@ -8,6 +8,7 @@
   config = lib.mkIf config.tsrk.bluetooth.enable {
     hardware.bluetooth = {
       enable = true;
+      powerOnBoot = true;
     };
 
     services.blueman.enable = true;
@@ -16,5 +17,15 @@
       bluez-tools
       blueberry
     ];
+
+    # Taken from https://github.com/Litarvan/legion/blob/main/modules/hardware/bluetooth.nix
+    services.pipewire.wireplumber.extraConfig = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
+    };
   };
 }
