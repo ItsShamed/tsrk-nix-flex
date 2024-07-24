1e390595372cfcda7552e327826036b0108fecbb
tsrk. <tsrk@tsrk.me>
2024-07-24 02:35:07 +0200

    chore(get_option): adapt command nixos-option call on NixOS systems
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- get_option.sh
+++ get_option.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -euo pipefail
 
 if [ $# -lt 2 ]; then
     echo "Usage: ./get_option.sh <host> <attrPath>" 1>&2
@@ -13,4 +14,12 @@ shift
 
 CONFIG_PREFIX="let flake = import $(nix flake metadata --json | jq .path -r); in flake.nixosConfigurations.$host"
 
-nix run nixpkgs#nixos-option -- --config_expr "${CONFIG_PREFIX}.config" --options_expr "${CONFIG_PREFIX}.options" "$attrPath" $@
+command=
+
+if command -v nixos-option &>/dev/null; then
+    command=nixos-option
+else
+    command=nix run nixpkgs#nixos-option --
+fi
+
+$command --config_expr "${CONFIG_PREFIX}.config" --options_expr "${CONFIG_PREFIX}.options" "$attrPath" $@
