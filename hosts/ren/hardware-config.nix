f35260d5ac2e6778f1c352a5233607050a6dba03
tsrk. <tsrk@tsrk.me>
2024-08-12 04:14:40 +0200

    fix(hosts: ren): disable elan_i2c kernel module
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- hosts/ren/hardware-config.nix
+++ hosts/ren/hardware-config.nix
@@ -6,6 +6,7 @@
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
+  boot.blacklistedKernelModules = [ "elan_i2c" ];
 
   networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
