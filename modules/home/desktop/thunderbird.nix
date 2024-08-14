fc6c30870b9f27002f035a5165dc8e1b797ee440
tsrk. <tsrk@tsrk.me>
2024-08-14 18:10:18 +0200

    refactor(home: desktop: thunderbird): use betterbird as the thunderbird package
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/home/desktop/thunderbird.nix
+++ modules/home/desktop/thunderbird.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   options = {
@@ -8,6 +8,7 @@
   config = lib.mkIf config.tsrk.thunderbird.enable {
     programs.thunderbird = {
       enable = lib.mkDefault true;
+      package = pkgs.betterbird;
       profiles."${config.home.username}" = {
         isDefault = true;
       };
