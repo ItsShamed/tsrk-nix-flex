74582731192e780327d1072b57ee76bf683b10ad
tsrk. <tsrk@tsrk.me>
2024-10-16 16:18:40 +0200

    fix(profiles: android: base): remove typo
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- profiles/android/base/default.nix
+++ profiles/android/base/default.nix
@@ -17,8 +17,9 @@
       "https://s3.cri.epita.fr/cri-nix-cache.s3.cri.epita.fr"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
-      "https://nix-on-droid.cachix.org";
+      "https://nix-on-droid.cachix.org"
     ];
+
     trustedPublicKeys = [
       "cache.nix.cri.epita.fr:qDIfJpZWGBWaGXKO3wZL1zmC+DikhMwFRO4RVE6VVeo="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
