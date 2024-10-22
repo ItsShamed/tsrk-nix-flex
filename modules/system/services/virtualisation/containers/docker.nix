e64ca2d908c4379c4ea08cbf5bdb34b00643fce1
tsrk. <tsrk@tsrk.me>
2024-10-22 13:27:43 +0200

    feat(system: services: virtualisation: containers: docker): use docker in rootless mode
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/system/services/virtualisation/containers/docker.nix
+++ modules/system/services/virtualisation/containers/docker.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 
+let
+  normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) (builtins.attrNames config.users.users);
+in
 {
   options = {
     tsrk.containers.docker.enable = lib.options.mkEnableOption "Docker";
@@ -8,7 +11,15 @@
   config = lib.mkIf config.tsrk.containers.docker.enable {
     tsrk.containers.enable = lib.mkDefault true;
 
-    virtualisation.docker.enable = true;
+    virtualisation.docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+    };
+
+    users.extraGroups.docker.members = normalUsers;
 
     environment.systemPackages = with pkgs; [
       docker-compose
