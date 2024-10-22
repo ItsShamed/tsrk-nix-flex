8ab6ca35cf2a006e2efe78022b6fa5f85dd4279c
tsrk. <tsrk@tsrk.me>
2024-10-22 13:30:36 +0200

    fix(system: services: virtualistaion: containers: podman): disable docker compat
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/system/services/virtualisation/containers/podman.nix
+++ modules/system/services/virtualisation/containers/podman.nix
@@ -9,7 +9,6 @@
     tsrk.containers.enable = lib.mkDefault true;
     virtualisation.podman = {
       enable = true;
-      dockerCompat = true;
 
       defaultNetwork.settings.dns_enabled = true;
       extraPackages = with pkgs; [
