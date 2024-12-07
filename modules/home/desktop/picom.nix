a92189d1656b504961a0d23f44cbb9dfc2d99d90
tsrk. <tsrk@tsrk.me>
2024-12-07 18:12:21 +0100

    feat(home: desktop: picom): add animations and migrate to window rules
    Doing this hard-coded because currently Home-Manager does not supports
    libconfig `lists`.
    See https://github.com/nix-community/home-manager/issues/5744
    
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/home/desktop/picom.nix
+++ modules/home/desktop/picom.nix
@@ -2,26 +2,34 @@
 
 let
   baseConfig = {
+    # TODO: Maybe reinstate module definition when HM will support specifying
+    # libconfig lists (not arrays), which is required to define animations
+    # See: https://github.com/nix-community/home-manager/issues/5744
     services.picom = {
       enable = true;
-      fade = true;
-      extraArgs = [ "--no-use-damage" ];
-      backend = "glx";
-      fadeDelta = 5;
-      fadeSteps = [ 0.075 0.030 ];
-      opacityRules = [
-        "100:class_g = 'kitty' && focused"
-        "75:class_g = 'kitty' && !focused"
-      ];
-      shadow = true;
-      shadowExclude = [
-        "class_g = 'Dunst'"
-        "class_g = 'Screenkey'"
-        "class_g = 'screenkey'"
-      ];
-      # TODO: Looking forward to put back animations when the new picom with
-      # animations get pacakged for nixpkgs
+      # fade = true;
+      # backend = "glx";
+      # fadeDelta = 5;
+      # fadeSteps = [ 0.075 0.030 ];
+      # opacityRules = [
+      #   "100:class_g = 'kitty' && focused"
+      #   "75:class_g = 'kitty' && !focused"
+      # ];
+      # shadow = true;
+      # shadowExclude = [
+      #   "class_g = 'Dunst'"
+      #   "class_g = 'Screenkey'"
+      #   "class_g = 'screenkey'"
+      # ];
     };
+    xdg.configFile."picom/picom.conf" = {
+      source = ./files/picom.conf;
+      text = lib.mkForce null;
+    };
+
+    warnings = [
+      "tsrk: Picom has been configured with a hard-coded file, completely discarding any configuration made from the Home-Manager module."
+    ];
   };
 
   picomCfg = config.services.picom;
