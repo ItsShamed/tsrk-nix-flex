214414444925cd16195e88e2a276fcf00b3a05d6
tsrk. <tsrk@tsrk.me>
2024-12-26 23:14:59 +0100

    refactor(system: services: x11: redshift): override birhgtness and temperature at night
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/system/services/x11/redshift.nix
+++ modules/system/services/x11/redshift.nix
@@ -11,9 +11,14 @@
     tsrk.redshift = { enable = lib.options.mkEnableOption "redshift"; };
   };
 
-  # Taken from https://gitlab.cri.epita.fr/cri/infrastructure/nixpie/-/blob/master/modules/services/x11/redshift.nix
+  # Derived from
+  # https://gitlab.cri.epita.fr/cri/infrastructure/nixpie/-/blob/master/modules/services/x11/redshift.nix
   config = lib.mkIf config.tsrk.redshift.enable {
-    services.redshift = { enable = true; };
+    services.redshift = {
+      enable = true;
+      temperature.night = 3000;
+      brightness.night = "0.5";
+    };
 
     # Used by redshift to determine sunrise and sunset.
     location = {
