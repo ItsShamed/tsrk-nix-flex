6228795729b98be79ec0378c094fe61bd2908665
tsrk. <tsrk@tsrk.me>
2025-01-21 13:34:53 +0100

    fix(system: services: hardware: bluetooth): ball in
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/system/services/hardware/bluetooth.nix
+++ modules/system/services/hardware/bluetooth.nix
@@ -27,10 +27,23 @@
       "10-bluez" = {
         "monitor.bluez.properties" = {
           "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = false;
+          "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" =
-            [ "a2dp_sink" "a2dp_source" "bap_sink" "bap_source" ];
+          "bluez5.roles" = [
+            "a2dp_sink"
+            "a2dp_source"
+            "bap_sink"
+            "bap_source"
+            "hsp_hs"
+            "hfp_hf"
+            "hsp_ag"
+            "hfp_ag"
+          ];
+        };
+      };
+      "11-bluetooth-policy" = {
+        "wireplumber.settings" = {
+          "bluetooth.autoswitch-to-headset-profile" = false;
         };
       };
     };
