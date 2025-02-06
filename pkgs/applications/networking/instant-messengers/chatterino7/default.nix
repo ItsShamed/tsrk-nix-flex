3a3431f468fcc31e019cc827f9087d6833481255
tsrk. <tsrk@tsrk.me>
2025-02-06 23:31:02 +0100

    fix(pkgs: chatterino7): add license header
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- pkgs/applications/networking/instant-messengers/chatterino7/default.nix
+++ pkgs/applications/networking/instant-messengers/chatterino7/default.nix
@@ -1,3 +1,9 @@
+# Copyright (c) 2025 tsrk. <tsrk@tsrk.me>
+# This file is licensed under the MIT license
+# See the LICENSE file in the repository root for more info.
+
+# SPDX-License-Identifier: MIT
+
 { chatterino2, lib, fetchFromGitHub }:
 
 chatterino2.overrideAttrs (self: super: {
