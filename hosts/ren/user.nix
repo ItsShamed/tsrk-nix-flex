1f117bb3a751c86d2f09abafdd309ab17b121a79
tsrk. <tsrk@tsrk.me>
2025-02-27 18:23:01 +0100

    feat(hosts: ren: user): add autorandr
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- hosts/ren/user.nix
+++ hosts/ren/user.nix
@@ -4,7 +4,7 @@
 
 # SPDX-License-Identifier: MIT
 
-{ self, ... }:
+{ self, pkgs, ... }:
 
 {
   imports = with self.homeManagerModules; [ profile-tsrk-private ];
@@ -32,5 +32,57 @@
       };
     };
     nvim.wakatime.enable = true;
+  };
+
+  services.autorandr.enable = true;
+  programs.autorandr = {
+    enable = true;
+    hooks.postswitch = { notify-i3 = "${pkgs.i3}/bin/i3-msg restart"; };
+    profiles = {
+      default = {
+        fingerprint = {
+          eDP-1 =
+            "00ffffffffffff0006af3d6800000000001d0104a51f117803b81aa6544a9b260e525500000001010101010101010101010101010101143780b87038244010103e0035ae100000180000000f0000000000000000000000000020000000fe0041554f0a202020202020202020000000fe004231343048414e30362e38200a00c8";
+        };
+        config = {
+          HDMI-1 = {
+            enable = false;
+            primary = false;
+          };
+          eDP-1 = {
+            enable = true;
+            primary = true;
+            mode = "1920x1080";
+          };
+          DP-1 = {
+            enable = false;
+            primary = false;
+          };
+        };
+      };
+      forge-docked-1080p = {
+        fingerprint = {
+          HDMI-1 =
+            "00ffffffffffff0026cd41618f090000181c010380351e782aee35a656529d280b5054b30c00714f818081c081009500b300d1c00101023a801871382d40582c45000f282100001e000000fd00374c1e5311000a202020202020000000ff0031313634384d38363032343437000000fc00504c32343933480a2020202020011902031ef14b9002030411121305141f01230907018301000065030c001000023a801871382d40582c45000f282100001e8c0ad08a20e02d10103e96000f2821000018011d007251d01e206e2855000f282100001e8c0ad090204031200c4055000f282100001800000000000000000000000000000000000000000000000000ab";
+          eDP-1 =
+            "00ffffffffffff0006af3d6800000000001d0104a51f117803b81aa6544a9b260e525500000001010101010101010101010101010101143780b87038244010103e0035ae100000180000000f0000000000000000000000000020000000fe0041554f0a202020202020202020000000fe004231343048414e30362e38200a00c8";
+        };
+        config = {
+          eDP-1 = {
+            enable = false;
+            primary = false;
+          };
+          HDMI-1 = {
+            enable = true;
+            primary = true;
+            mode = "1920x1080";
+          };
+          DP-1 = {
+            enable = false;
+            primary = false;
+          };
+        };
+      };
+    };
   };
 }
