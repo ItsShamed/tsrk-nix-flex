266745fcd390ff26cdbb90d6f165c1395bd11f0b
tsrk. <tsrk@tsrk.me>
2025-04-05 03:09:00 +0200

    fix(home: desktop: rofi): remove rbw from plugins
    it's not a plugin
    
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/home/desktop/rofi.nix
+++ modules/home/desktop/rofi.nix
@@ -18,7 +18,7 @@ in {
     programs.rofi = {
       enable = true;
       package = pkgs.rofi-wayland;
-      plugins = with pkgs; [ rofi-emoji-wayland rofi-rbw ];
+      plugins = with pkgs; [ rofi-emoji-wayland ];
       theme = "${tsrkPkgs.rofi-themes-collection}/simple-tokyonight.rasi";
       terminal = (self.lib.mkIfElse (config.programs.kitty.enable) "kitty"
         "${pkgs.alacritty}/bin/alacritty");
