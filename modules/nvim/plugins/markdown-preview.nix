9d0ec6f72b30f598d63dd3431c4c3d8699ec3892
tsrk. <tsrk@tsrk.me>
2025-04-19 14:00:44 +0200

    style(nvim: plugins: markdown-preview): put browser warning in `warnings`
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/nvim/plugins/markdown-preview.nix
+++ modules/nvim/plugins/markdown-preview.nix
@@ -7,12 +7,11 @@
 { lib, ... }:
 
 {
+  warnings = [
+    "markdown-preview browser is set to 'librewolf', if it's not what you want, please change `plugins.markdown-preview.settings.browser`"
+  ];
   plugins.markdown-preview = {
     enable = true;
-    settings = {
-      browser = lib.mkDefault (lib.warn
-        "markdown-preview browser is set to 'librewolf', if it's not what you want, please change `plugins.markdown-preview.settings.browser`"
-        "librewolf");
-    };
+    settings.browser = lib.mkDefault "librewolf";
   };
 }
