0aea669f8225f8686d2684e5a2f6f1706ef3a992
tsrk. <tsrk@tsrk.me>
2025-04-29 14:51:50 +0200

    refactor(hosts: ren): use active pstate
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- hosts/ren/default.nix
+++ hosts/ren/default.nix
@@ -51,11 +51,11 @@
     CPU_BOOST_ON_AC = 1;
     CPU_BOOST_ON_BAT = 0;
 
-    CPU_DRIVER_OPMODE_ON_AC = "guided";
-    CPU_DRIVER_OPMODE_ON_BAT = "passive";
+    CPU_DRIVER_OPMODE_ON_AC = "active";
+    CPU_DRIVER_OPMODE_ON_BAT = "active";
 
-    CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
-    CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
     MEM_SLEEP_ON_AC = "s2idle";
     MEM_SLEEP_ON_BAT = "deep";
