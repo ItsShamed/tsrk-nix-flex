8992e17fb3975dd3ecedc42f87d746797ed38ed9
tsrk. <tsrk@tsrk.me>
2025-05-02 17:27:52 +0200

    refactor(overlays: retroarch): remove some cores
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- overlays/retroarch/default.nix
+++ overlays/retroarch/default.nix
@@ -9,21 +9,13 @@ self: super:
 {
   retroarch = super.retroarch.override {
     cores = with super.libretro; [
-      gw
-      beetle-gba
-      bsnes
       citra
-      desmume
       dolphin
       dosbox
       gpsp
-      mame
       melonds
-      mesen
-      meteor
       mgba
       mupen64plus
-      pcsx2
       ppsspp
       sameboy
       snes9x
