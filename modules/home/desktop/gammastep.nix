8236c78457544eabce87a64b1552a84e01ad6d26
tsrk. <tsrk@tsrk.me>
2025-07-26 13:23:46 +0200

    feat(home: desktop: gammastep): enable verbose logging
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- modules/home/desktop/gammastep.nix
+++ modules/home/desktop/gammastep.nix
@@ -17,6 +17,7 @@ in {
   config = lib.mkIf cfg.enable {
     services.gammastep = {
       enable = lib.mkDefault true;
+      enableVerboseLogging = lib.mkDefault true;
       settings.general.brightness-night = 0.85;
       temperature.night = 3000;
       latitude = 48.87951;
