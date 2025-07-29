df9dae427b1af79cea9858c1459f679717de2c18
tsrk. <tsrk@tsrk.me>
2025-07-29 21:33:23 +0200

    style(overlays: sddm): use passed-through unwrapped derivation
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- overlays/imports.nix
+++ overlays/imports.nix
@@ -4,4 +4,4 @@
 
 # SPDX-License-Identifier: MIT
 
-[ ./retroarch ./rofi-power-menu ./sound-theme-freedesktop { path = ./sddm; } ]
+[ ./retroarch ./rofi-power-menu ./sound-theme-freedesktop ./sddm ]
--- overlays/sddm/default.nix
+++ overlays/sddm/default.nix
@@ -4,24 +4,17 @@
 
 # SPDX-License-Identifier: MIT
 
-{ inputs, ... }:
-
 self: super:
 
 super.lib.genAttrs [ "libsForQt5" "kdePackages" ] (p:
-  super.${p}.overrideScope (pself: psuper:
-    let
-      unwrapped = psuper.callPackage
-        "${inputs.nixpkgs}/pkgs/applications/display-managers/sddm/unwrapped.nix"
-        { };
-    in {
-      sddm = psuper.sddm.override {
-        unwrapped = unwrapped.overrideAttrs (final: old: {
-          patches = (old.patches or [ ]) ++ [
-            ./patches/0001-fix-helper-do-not-badly-exit-on-SIGHUP.patch
-            ./patches/0002-fix-helper-avoid-exiting-1-at-all-costs-when-session.patch
-            ./patches/0003-feat-helper-log-session-stdout-and-stderr.patch
-          ];
-        });
-      };
-    }))
+  super.${p}.overrideScope (pself: psuper: {
+    sddm = psuper.sddm.override {
+      unwrapped = psuper.sddm.unwrapped.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          ./patches/0001-fix-helper-do-not-badly-exit-on-SIGHUP.patch
+          ./patches/0002-fix-helper-avoid-exiting-1-at-all-costs-when-session.patch
+          ./patches/0003-feat-helper-log-session-stdout-and-stderr.patch
+        ];
+      });
+    };
+  }))
