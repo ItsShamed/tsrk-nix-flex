df74e4332a50ad0ed0b822c5363e2c19442498ab
tsrk. <tsrk@tsrk.me>
2026-06-19 21:04:55 +0200

    feat(hosts: taku): install jdk25
    For that fuckass cleanroom shit
    
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- hosts/taku/default.nix
+++ hosts/taku/default.nix
@@ -103,7 +103,10 @@ in
 
   tsrk.gns3.enable = true;
 
-  tsrk.packages.pkgs.java.jdk.extraPackages = with pkgs; [ temurin-jre-bin-17 ];
+  tsrk.packages.pkgs.java.jdk.extraPackages = with pkgs; [
+    temurin-jre-bin-17
+    jdk25
+  ];
 
   time.hardwareClockInLocalTime = true;
 
