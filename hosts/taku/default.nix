d2bb1f78268ed7bd6bd170609bfb166809ec5869
tsrk. <tsrk@tsrk.me>
2026-08-28 02:57:01 +0200

    fix(hosts: taku): target right PCI device when disabling ACPI wakeup
    Signed-off-by: tsrk. <tsrk@tsrk.me>

--- hosts/taku/default.nix
+++ hosts/taku/default.nix
@@ -153,7 +153,7 @@ in
     '')
     # This prevent locking up when suspending
     (writeTextDir "etc/udev/rules.d/14-fix-broken-xhci" ''
-      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x1022", ATTR{device}=="0x15b6", ATTR{power/wakeup}="disabled"
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x1022", ATTR{device}=="0x14e3", ATTR{power/wakeup}="disabled"
     '')
     sane-airscan
   ];
